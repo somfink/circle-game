34642a66a39e38905b4925f1a72af072fac8fb86
Kacper Gajewski <kacper.gajewski@op.pl>
2023-04-01 11:33:57 +0200

    feat: added enemy circles with random sizes and positions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { MouseEventHandler, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { MouseCircle } from './components/MouseCircle/MouseCircle';
 import './App.scss';
+import { EnemyCircles } from './components/EnemyCircles/EnemyCircles';
 
 function App() {
     const [mousePos, setMousePos] = useState<{ x: number; y: number }>();
     const [circlePos, setCirclePos] = useState({
         top: 50,
         left: 50,
+    });
+    const [enemyCirclePos, setEnemyCirclePos] = useState({
+        top: 500,
+        left: 500,
+        size: 4,
     });
 
     // circle size is calculated as 8rem * 12px, cause 1rem = 12px
@@ -15,8 +22,8 @@ function App() {
         setMousePos({ x: e.clientX, y: e.clientY });
 
         setCirclePos({
-            top: e.clientY - (circleSize / 2) ,
-            left: e.clientX - (circleSize / 2),
+            top: e.clientY - circleSize / 2,
+            left: e.clientX - circleSize / 2,
         });
     };
 
@@ -28,15 +35,43 @@ function App() {
         };
     }, [mousePos]);
 
+    // setInterval(() => {
+    //     const random = Math.random();
+    //     if (random > 0.25 && random <= 0.5) {
+    //         setEnemyCirclePos(prevState => ({
+    //             top: prevState.top,
+    //             left: prevState.left - 1,
+    //             size: 4,
+    //         }));
+    //         return;
+    //     }
+    //     if (random > 0.5 && random <= 0.75) {
+    //         setEnemyCirclePos(prevState => ({
+    //             top: prevState.top,
+    //             left: prevState.left + 1,
+    //             size: 4,
+    //         }));
+    //         return;
+    //     }
+    //     if (random > 0.75) {
+    //         setEnemyCirclePos(prevState => ({
+    //             top: prevState.top - 1,
+    //             left: prevState.left,
+    //             size: 4,
+    //         }));
+    //         return;
+    //     }
+    //     setEnemyCirclePos(prevState => ({
+    //         top: prevState.top + 1,
+    //         left: prevState.left,
+    //         size: 4,
+    //     }));
+    // }, 5000);
+
     return (
         <main className="main-container">
-            <div
-                className="main-circle"
-                style={{
-                    top: `${circlePos.top}px`,
-                    left: `${circlePos.left}px`,
-                }}
-            ></div>
+            <MouseCircle circlePosition={circlePos} />
+            <EnemyCircles />
         </main>
     );
 }
